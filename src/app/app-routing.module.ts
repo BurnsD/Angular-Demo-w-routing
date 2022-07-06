77508cb2ade6188939c2533052ebe2833080ff8a
BurnsD <Dustinburnsactech@gmail.com>
2022-07-05 20:26:29 -0500

    added basic routing to hardcoded header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { EmployeeTableComponent } from './employee-table/employee-table.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { SimpleCalculatorComponent } from './simple-calculator/simple-calculator.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'employee', component: EmployeeComponent },
+  { path: 'employee-table', component: EmployeeTableComponent },
+  { path: 'calculator', component: CalculatorComponent },
+  { path: 'simple-calculator', component: SimpleCalculatorComponent },
+  { path: 'home', component: LoginFormComponent},
+  { path: '', component: HeaderComponent},
+  { path: '', component: FooterComponent},
+
+
+  { path: 'search/:searchTerm', component: EmployeeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+export const routingComponents = [
+  EmployeeComponent,
+  EmployeeTableComponent,
+  CalculatorComponent,
+  SimpleCalculatorComponent,
+  HeaderComponent,
+  FooterComponent
+
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { EmployeeTableComponent } from './employee-table/employee-table.component';
-import { EmployeeComponent } from './employee/employee.component';
-import { SimpleCalculatorComponent } from './simple-calculator/simple-calculator.component';
-import { CalculatorComponent } from './calculator/calculator.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    EmployeeTableComponent,
-    EmployeeComponent,
-    SimpleCalculatorComponent,
-    CalculatorComponent
+    routingComponents,
+    LoginFormComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
+
